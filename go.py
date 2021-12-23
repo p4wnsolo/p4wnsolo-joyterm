b4626a9f2b7e71e8ccf220949f2db31153980e87
PawnSolo <94819104+p4wnsolo@users.noreply.github.com>
2021-12-23 08:31:06 -0700

    Fixed file path issue

--- go.py
+++ go.py
@@ -2,20 +2,23 @@
 # -*- coding:utf-8 -*-
 
 import subprocess
-
+import pathlib
 from pathlib import Path
+
+prefix = str(pathlib.Path(__file__).parent.resolve()) + '/'
 home = str(Path.home())  # Get home folder (makes it cross-platform)
 
 print('\nHome: ' + home + '\n')
+print('This folder: ' + prefix + '\n')
 
 ### Script A
 a1 = '/usr/bin/python3'
-a2 = home + '/p4wnsolo/p4wnsolo-joyterm/p4wnsolo-joytext.py'
+a2 = prefix + 'p4wnsolo-joytext.py'
 subprocess.call([a1, a2])  # Run script
 
 ### Script B
 b1 = '/usr/bin/python3'
-b2 = home + '/p4wnsolo/p4wnsolo-joyterm/p4wnsolo-terminal.py'
+b2 = prefix + 'p4wnsolo-terminal.py'
 b3 = '-i'
 b4 = 'spi'
 b5 = '--display'
